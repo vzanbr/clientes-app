0d9eee863fcfe1307f7baa2129cd457f4fe35657
vzanbr <gabriecvr123@gmail.com>
2024-03-13 16:08:17 -0300

    feat : component interface create

---
+++ src/app/interface/interface.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interface',
+  templateUrl: './interface.component.html',
+  styleUrls: ['./interface.component.css']
+})
+export class InterfaceComponent {
+
+}
