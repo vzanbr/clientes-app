149353cdb13b7e50c33dbd53688d9aa4df41806b
vzanbr <gabriecvr123@gmail.com>
2024-03-13 16:41:18 -0300

    feat : Começando no array, demostrando no html

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,26 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'hello',
-    template: `
-        <h1>Hello word</h1>
-    `
+    templateUrl: './hello.component.html'
 })
 export class HelloComponent{
 
+    nome: string;
+    clientes : Cliente[];
+
+    constructor() {
+        this.nome = 'gabriel';
+        let cliente = new Cliente('Gabriel', 24);
+        let cliente2 = new Cliente('Mane', 15)
+        let cliente3 = new Cliente('Mane2', 150)
+        this.clientes = [cliente, cliente2, cliente3];
+    }
+}
+
+class Cliente {
+
+    constructor(
+        public nome: string,
+        public idade: number
+    ) {}
 }
